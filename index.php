dd259f82b699c1c3c78fe49318f2e3894377b3d3
Dmitry Nikulin <theowl@etersoft.ru>
2017-11-21 12:15:44 +0300

    Improve test script: use local copy, preserve form value after sending

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<script type="text/javascript" src="//eterfund.ru/js/jquery/latest/jquery.min.js"></script>
-		<script type="text/javascript" src="//eterfund.ru/api/email/svcCheckEmail.js"></script>
+		<script type="text/javascript" src="svcCheckEmail.js"></script>
 		<style type="text/css">
 			form {
 				width: 100%;
@@ -15,7 +15,7 @@
 </html>
 <body>
 	<form action="" method="post">
-		E-mail: <input type="text" name="email" value="" class="svcCheckEmail" />
-		<input type="button" value="Отправить" />
+		E-mail: <input type="text" name="email" value="<?php echo $_POST['email']; ?>" class="svcCheckEmail" />
+		<input type="submit" value="Отправить" />
 	</form>
 </body>
